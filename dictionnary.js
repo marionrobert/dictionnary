18698a735bc567adbbbf581db5fb3ab6b7def425
Marion Robert <mrobert148@gmail.com>
2024-03-04 12:13:22 -0500

    refactorisation with reduce function

--- dictionnary.js
+++ dictionnary.js
@@ -1,13 +1,18 @@
-function matchString(string) {
+function splitString(string) {
   return string.match(/[^ ]+/g)
 }
 
 module.exports = function dictionary(words){
-  const wordsList = matchString(words) || []
-  const result = {}
+  // const wordsList = splitString(words) || []
+  // const result = {}
+  // wordsList.forEach(word => {
+  //   result[word] = result[word] + 1 || 1
+  // });
+  // return result;
 
-  wordsList.forEach(word => {
-    result[word] = result[word] + 1 || 1
-  });
-  return result;
+  // with refactorisation
+  return (splitString(words) || []).reduce((current, word) => {
+    current[word] = current[word] + 1 || 1;
+    return current;
+  }, {})
 }
